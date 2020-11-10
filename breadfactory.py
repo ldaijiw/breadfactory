301c27af9bee775fd13ab6b8aa25a6d2da3c7a28
ldaijiw <lwaltmann@spartaglobal>
2020-11-10 16:00:33 +0000

    added list comprehension to switch each item to lowercase in ingredients

--- breadfactory.py
+++ breadfactory.py
@@ -2,21 +2,33 @@ class BreadFactory:
     
     def make_dough(self, *ingredients):
         print("\nMAKING DOUGH")
+        # change all items to lowercase
+        ingredients = [item.lower() for item in ingredients]
+        # will only return dough if water and flour contained ingredient
         if 'water' in ingredients and 'flour' in ingredients:
             return 'dough'
         return 'Water and flour needed to make dough'
     
-    
-    def bake_dough(self, ingredient):
+
+    def bake_dough(self, *ingredients):
         print("\nBAKING")
-        if ingredient == 'dough':
+        # change all items to lowercase
+        ingredients = [item.lower() for item in ingredients]
+        # will only return bread if dough contained in ingredients
+        if 'dough' in ingredients:
             return 'bread'
         return 'dough not supplied'
 
 
     def run_factory(self, *ingredients):
+        # change all items to lowercase
+        ingredients = [item.lower() for item in ingredients]
+        
+        # will attempt to make and bake dough
         product = self.make_dough(*ingredients)
         baked_product = self.bake_dough(product)
+
+        # returns True only if final product is bread
         if baked_product == 'bread':
             print("\nSUCCESSFULLY PRODUCED BREAD")
             return True
